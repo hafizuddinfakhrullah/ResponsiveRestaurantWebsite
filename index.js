3a7f5f8bdb7ffc2940fddbd652836cc710d1344e
HAFIZUDDIN FAKHRULLAH SUHAIMI <95210746+hafizuddinfakhrullah@users.noreply.github.com>
2023-11-11 00:38:53 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+// showing nav on mobile version
+const mobile = document.querySelector('.menu-toggle');
+const mobileLink = document.querySelector('.sidebar');
+
+mobile.addEventListener('click', function() {
+    mobile.classList.toggle('is-active');
+    mobileLink.classList.toggle('active');
+})
+
+// close menu when click
+mobileLink.addEventListener('click', function() {
+    const menuBars = document.querySelector('.is-active');
+    if(window.innerWidth<=768 && menuBars) {
+        mobile.classList.toggle('is-active');
+        mobileLink.classList.toggle('active');
+    }
+})
+
+// move the menu to right and left when click back and next
+var step = 100;
+var stepfilter = 60;
+var scrolling = true;
+
+$('.back').bind('click', function(e) {
+    e.preventDefault();
+    $('.highlight-wrapper').animate({
+        scrollLeft: "-=" + step + "px"
+    });
+});
+
+$('.next').bind('click', function(e) {
+    e.preventDefault();
+    $('.highlight-wrapper').animate({
+        scrollLeft: "+=" + step + "px"
+    })
+})
+
+// when click back and next on the menu filter
+$(".back-menus").bind("click", function(e){
+    e.preventDefault();
+    $(".filter-wrapper").animate({
+        scrollLeft: "-=" + stepfilter + "px"
+    })
+});
+
+$(".next-menus").bind("click", function(e){
+    e.preventDefault();
+    $(".filter-wrapper").animate({
+        scrollLeft: "+=" + stepfilter + "px"
+    })
+});
+
+
